6aff40a95c74295de9bb475b3359205a06dbca50
zhenhailong <wangz3@illinois.edu>
2025-03-31 14:26:17 -0500

    update llava ov before token packing

--- run_scripts/run_ov_eval0.sh
+++ run_scripts/run_ov_eval0.sh
@@ -1,9 +1,10 @@
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=2
 ## add openai api key to use gpt as judge ##
 export HF_TOKEN=$(cat /shared/nas2/wangz3/salesforce_intern_nas2/hf_token)
 export OPENAI_API_KEY=$(cat /shared/nas2/wangz3/salesforce_intern_nas2/openai_api_key_ecole)
 export TMPDIR=/scratch/wangz3/tmp
 echo "Using tmp dir: $TMPDIR"
 
-# 81out instance-level + token repeating
-python run.py --config "run_configs/llava-ov-new-repeat/instance_level_81out_llava_ov_7b-si_repeat_all.json" --reuse --judge "gpt-4o-mini"
+python run.py --config "run_configs/efficiency_run/check_efficiency__apply_thresh_81out_llava_ov_7b-si_repeat_all.json" --reuse --judge "gpt-4o-mini"
+python run.py --config "run_configs/efficiency_run/check_efficiency__apply_thresh_162out_llava_ov_7b-si_repeat_all.json" --reuse --judge "gpt-4o-mini"
+
--- run_scripts/run_ov_test.sh
+++ run_scripts/run_ov_test.sh
@@ -1,8 +1,10 @@
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=3
 ## add openai api key to use gpt as judge ##
 export HF_TOKEN=$(cat /shared/nas2/wangz3/salesforce_intern_nas2/hf_token)
 export OPENAI_API_KEY=$(cat /shared/nas2/wangz3/salesforce_intern_nas2/openai_api_key_ecole)
 export TMPDIR=/scratch/wangz3/tmp
 echo "Using tmp dir: $TMPDIR"
 
-python run.py --config "run_configs/run_missing/apply_thresh_81out_llava_ov_7b-si_no_repeating_part_3.json" --reuse --judge "gpt-4o-mini"
+# 81out instance-level + token repeating
+# python run.py --config "run_configs/test_MMMU.json" --reuse --judge "gpt-4o-mini"
+python run.py --config "run_configs/test_video.json" --reuse --judge "gpt-4o-mini"
